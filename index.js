eda71724ce059a370f028033ceab35bd24917f03
Miguel Mendoza <miguel.mendoza8602@gmail.com>
2024-11-20 10:16:01 -0600

    added files

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-var rs = require("readline-sync");
-var theNumbers = [];
+const rs = require("readline-sync");
 
-var whatOperation = rs.question("What operation would you like to perform? ");
+const whatOperation = rs.question("What operation would you like to perform? ");
+const firstNumber = rs.questionInt("Please enter the first number ");
+const secondNumber = rs.questionInt("Please enter the second number ");
+
+function calculate(num1, num2, operator) {
+  if (operator === "+") {
+    return num1 + num2;
+  }
+  if (operator === "-") {
+    return num1 - num2;
+  }
+  if (operator === "*") {
+    return num1 * num2;
+  }
+  if (operator === "/") {
+    return num1 / num2;
+  }
+}
 
 if (
   whatOperation === "+" ||
@@ -11,40 +27,24 @@ if (
 ) {
   if (whatOperation === "+") {
     function runPlus() {
-      var firstNumber = rs.questionInt("Please enter the first number ");
-      theNumbers.push(firstNumber);
-      var secondNumber = rs.questionInt("Please enter the second number ");
-      theNumbers.push(secondNumber);
       console.log("The result is", firstNumber + secondNumber);
     }
     runPlus();
   }
   if (whatOperation === "-") {
     function runSub() {
-      var firstNumber = rs.questionInt("Please enter the first number ");
-      theNumbers.push(firstNumber);
-      var secondNumber = rs.questionInt("Please enter the second number ");
-      theNumbers.push(secondNumber);
       console.log("The result is", firstNumber - secondNumber);
     }
     runSub();
   }
   if (whatOperation === "/") {
     function runDiv() {
-      var firstNumber = rs.questionInt("Please enter the first number ");
-      theNumbers.push(firstNumber);
-      var secondNumber = rs.questionInt("Please enter the second number ");
-      theNumbers.push(secondNumber);
       console.log("The result is", firstNumber / secondNumber);
     }
     runDiv();
   }
   if (whatOperation === "*") {
     function runMulti() {
-      var firstNumber = rs.questionInt("Please enter the first number ");
-      theNumbers.push(firstNumber);
-      var secondNumber = rs.questionInt("Please enter the second number ");
-      theNumbers.push(secondNumber);
       console.log("The result is", firstNumber * secondNumber);
     }
     runMulti();
