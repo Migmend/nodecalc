da265b0f1f80849ffff9351801b79e7b0d7588ec
Miguel Mendoza <miguel.mendoza8602@gmail.com>
2024-12-02 20:00:46 -0600

    added files fixed first question

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const rs = require("readline-sync");
 
-const whatOperation = rs.question("What operation would you like to perform? ");
-const firstNumber = rs.questionInt("Please enter the first number ");
-const secondNumber = rs.questionInt("Please enter the second number ");
+const operations = ["+", "-", "*", "/"];
+function firstQuestion() {
+  let whatOperation = rs.question("what operation would you like to perform? ");
+  if (operations.includes(whatOperation)) {
+    console.log(whatOperation);
+    return whatOperation;
+  } else {
+    console.log("that is not a valid operator");
+    firstQuestion();
+  }
+}
+
+firstQuestion();
 
 function calculate(num1, num2, operator) {
   if (operator === "+") {
@@ -19,41 +29,10 @@ function calculate(num1, num2, operator) {
   }
 }
 
-if (
-  whatOperation === "+" ||
-  whatOperation === "-" ||
-  whatOperation === "/" ||
-  whatOperation === "*"
-) {
-  if (whatOperation === "+") {
-    function runPlus() {
-      console.log("The result is", firstNumber + secondNumber);
-    }
-    runPlus();
-  }
-  if (whatOperation === "-") {
-    function runSub() {
-      console.log("The result is", firstNumber - secondNumber);
-    }
-    runSub();
-  }
-  if (whatOperation === "/") {
-    function runDiv() {
-      console.log("The result is", firstNumber / secondNumber);
-    }
-    runDiv();
-  }
-  if (whatOperation === "*") {
-    function runMulti() {
-      console.log("The result is", firstNumber * secondNumber);
-    }
-    runMulti();
-  }
-} else if (
-  whatOperation != "+" ||
-  whatOperation != "-" ||
-  whatOperation != "/" ||
-  whatOperation != "*"
-) {
-  console.log("That is not a valid operation");
-}
+const firstNumber = rs.questionInt("Please enter the first number ");
+const secondNumber = rs.questionInt("Please enter the second number ");
+
+console.log(
+  "The result is ",
+  calculate(firstNumber, secondNumber, firstQuestion())
+);
